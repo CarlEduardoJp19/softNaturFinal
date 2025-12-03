8b67100f8c50b8e67870f1b1e5f1eb205145a778
EduarJP <eduarjp2502@gmail.com>
2025-12-03 08:59:43 -0500

    Agregando dirs staticfield

--- Config/settings.py
+++ Config/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = []
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
